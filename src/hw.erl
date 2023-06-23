8f450b18ce62b3f99da7cc787c6cbf835e559905
Safi Ullah <41166092+saffeullah@users.noreply.github.com>
2023-06-23 19:23:01 -0400

    Hello World - Environment Testing

---
+++ src/hw.erl
@@ -0,0 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @author safi
+%%% @copyright (C) 2023, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. Jun. 2023 7:17 p.m.
+%%%-------------------------------------------------------------------
+-module(hw).
+-author("safi").
+
+%% API
+-export([ helloworld/0]).
+
+helloworld() ->
+  "Hello this is the world".
+
